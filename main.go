a8cffd42729a8ba51cd301b06f04e34bb5219fcd
Malka123456 <malka988276@gmail.com>
2026-02-13 16:28:16 +0530

    transferred routes to a separate file routes.go

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"learning-backend/database"
-	"learning-backend/handlers"
+	//"learning-backend/handlers"
 	//"learning-backend/models"
 
 	"fmt"
@@ -17,17 +17,7 @@ func main() {
 
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Backend is running")
-	})
-
-	app.Get("/health", func(c *fiber.Ctx) error {
-		return c.SendString("Server is healthy")
-	})
 	
-  app.Post("/register", handlers.CreateUser)
-
-
 	fmt.Println("server is running")
 
 	app.Listen(":3000")
--- routes/routes.go
+++ routes/routes.go
@@ -1,5 +1,24 @@
 package routes
 
-func SetupRoutes() {
+import (
+	"learning-backend/handlers"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupRoutes(app *fiber.App) {
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Backend is running")
+	})
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.SendString("Server is healthy")
+	})
 	
+  app.Post("/register", handlers.CreateUser)
+
+
+
+
 }
